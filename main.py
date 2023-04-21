7c62fac0b77251313dd09e964e2cca1dc267d20f
Tom Gerard <tom.gerard@gmail.com>
2023-04-21 10:08:11 -0500

    added frame example

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def continuous_beam_example():
     nodes[1].set_to_roller_support()
     nodes[2].set_to_pin_support()
     nodes[4].set_to_pin_support()
-    nodes[6].set_constraints(True, True, True)  # fixed
+    nodes[6].set_constraints(True, True, True)  # free
 
     # check that node ids assigned properly
     for node in nodes:
@@ -67,6 +67,61 @@ def continuous_beam_example():
     print("Global Stiffness Matrix")
     print('\n'.join([' '.join(['{:8}'.format(item) for item in row]) 
       for row in k_global]))
+    
+def frame_example():
+  """
+  Example 5.3 from "Matrix Structural Analysis: Second Edition"
+  by William McGuire
+  """
+
+  # nodes
+  nodes = Node2DList()
+  nodes.append(Node2D(Point2D(0, 0)))
+  nodes.append(Node2D(Point2D(0, 5)))
+  nodes.append(Node2D(Point2D(7.416, 8)))
+  nodes.append(Node2D(Point2D(11.416, 5)))
+  nodes.append(Node2D(Point2D(11.416, 0)))
+
+  # assign support conditions
+  nodes[0].set_constraints(False, False, False)  # fixed
+  nodes[4].set_constraints(False, False, False)  # fixed
+
+  elastic_mod = 200_000
+  area_1 = 4_000
+  inertia_1 = 50_000
+  area_2 = 6_000
+  inertia_2 = 200_000_000
+
+  # create element list
+  elements = [
+      Beam2D(nodes[0], nodes[1], area_1, inertia_1, elastic_mod),  # ab
+      Beam2D(nodes[1], nodes[2], area_2, inertia_2, elastic_mod),  # bc
+      Beam2D(nodes[2], nodes[3], area_1, inertia_1, elastic_mod),  # cd
+      Beam2D(nodes[3], nodes[4], area_1, inertia_1, elastic_mod)   # de
+  ]
+
+  elements[0].set_ID(0)
+  elements[1].set_ID(1)
+  elements[2].set_ID(2)
+  elements[3].set_ID(3)
+
+  structure = Structure(StructureType.PLANE_FRAME,nodes, elements)
+
+  map = structure.get_dof_map()
+
+  print()
+  print("Degree of Freedom Map")
+  print('\n'.join([' '.join(['{:4}'.format(item) for item in row]) 
+    for row in map]))
+  
+  assembler = Assembler(structure)
+  k_global = assembler.assemble()
+
+  print()
+  print("Global Stiffness Matrix")
+  print('\n'.join([' '.join(['{:8}'.format(item) for item in row]) 
+    for row in k_global]))
+
 
 if __name__ == "__main__":
     continuous_beam_example()
