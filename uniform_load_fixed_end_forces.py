66a5e95d9fb03438df213c142bfe0c42a848eac8
Tom Gerard <tom.gerard@gmail.com>
2023-04-24 12:06:22 -0500

    added uniform_load_fixed_end_forces.py

---
+++ uniform_load_fixed_end_forces.py
@@ -0,0 +1,32 @@
+from beam_element import Beam2D
+
+
+class UniformFixedEndForces:
+
+    def __init__(self, beam: Beam2D, load: float) -> None:
+        self.beam = beam
+        self.load = load
+    
+    def fixed_end_moment_left(self):
+        """
+        Equivalent nodal moment on the left end of a fixed end beam.
+        """
+        return self.load * self.beam.length ** 2 / 12.0
+    
+    def fixed_end_moment_right(self):
+        """
+        Equivalent nodal moment on the right end of a fixed end beam.
+        """
+        return self.load * self.beam.length ** 2 / 12.0
+    
+    def fixed_end_shear_left(self):
+        """
+        Equivalent nodal shear force on the left end of a fixed end beam.
+        """
+        return self.load * self.beam.length / 2.0
+    
+    def fixed_end_shear_right(self):
+        """
+        Equivalent nodal shear force on the right end of a fixed end beam.
+        """
+        return self.load * self.beam.length / 2.0
