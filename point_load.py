dea0c83df90da4c6798feca96175da3249350073
Tom Gerard <tom.gerard@gmail.com>
2023-05-08 18:50:29 -0500

    Added point_load.py

---
+++ point_load.py
@@ -0,0 +1,21 @@
+from point_load_fixed_end_forces import PointLoadFixedEndForces
+
+
+class PointLoad2D:
+    def __init__(self, force: float, distance: float) -> None:
+        self.force = force
+        self.set_distance(force)
+    
+    def set_distance(self, distance: float):
+        """
+        Set the distance as a ratio of the element's length. Zero
+        indicates the start of the element, and one indicates the
+        end of an element.
+        """
+        if distance < 0 or distance > 1:
+            raise ValueError('Distance must be a ratio of element length.')
+        else:
+            self.__distance = distance
+    
+    def get_distance(self):
+        return self.__distance
